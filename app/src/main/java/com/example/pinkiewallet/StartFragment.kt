6b214ab9c0e3437aeb7cd3866408e39e6ee907e8
Juan Nathaniel <joannanthaniel@gmail.com>
2024-06-29 01:54:50 +0700

    New Feature : Start App

---
+++ app/src/main/java/com/example/pinkiewallet/StartFragment.kt
@@ -0,0 +1,39 @@
+package com.example.pinkiewallet
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pinkiewallet.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Menggunakan binding yang sesuai untuk button
+        binding.lanjutkanButton.setOnClickListener {
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+    }
+}
